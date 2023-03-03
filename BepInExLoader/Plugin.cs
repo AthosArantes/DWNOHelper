c7fc7f50d7e5be2c8791022033712280fad7faf7
AthosArantes <45997893+AthosArantes@users.noreply.github.com>
2023-03-03 19:26:50 -0300

    Add BepInEx loader.

---
+++ BepInExLoader/Plugin.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using System.Runtime.InteropServices;
+
+namespace DWNOHelper.ManagedLoader;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BasePlugin
+{
+	public override void Load()
+	{
+		try
+		{
+			var hmodule = NativeLibrary.Load(@"BepInEx\plugins\DWNOHelper.dll");
+			// Plugin startup logic
+			Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+		}
+		catch (System.Exception e)
+		{
+			Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} failed to load: {e.Message}");
+		}
+	}
+}
